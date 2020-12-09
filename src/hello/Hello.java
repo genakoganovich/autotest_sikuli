2bef8ffccfb9c036a3448f67ad38ba3c22a67a39
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-12-09 18:43:34 +0200

    0001 addDB() refactoring - parameters added

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -6,9 +6,6 @@ import org.sikuli.script.KeyModifier;
 
 public class Hello {
     public static final String DB_NAME = "Autotest";
-    public static final String DB_STRUCTURE_PATH = "C:/data/Autotest/__DB__/structure/";
-    public static final String DB_DATA_PATH = "C:/data/Autotest/__DB__/data/";
-    public static final String DB_STORAGE_PATH = "C:/data/Autotest/__STORAGE__/";
     public static final String DB_STORAGE_DESCRIPTION = "STORAGE";
 
     public static final String ADD_DB_BUTTON_IMAGE = "c:/Users/gennady/eclipse-workspace/001_images/0001_Add_DB_button.png";
@@ -23,10 +20,10 @@ public class Hello {
 
     public static void main(String[] args) {
         Screen s = new Screen();
-        Hello.addDB(s);
+        Hello.addDB(s, DB_NAME, "C:/data/", DB_STORAGE_DESCRIPTION);
     } // main
 
-    public static void addDB(Screen s, String dbName, String folder) {
+    public static void addDB(Screen s, String dbName, String folder, String dbStorageDescription) {
         try {
             s.paste(ADD_DB_BUTTON_IMAGE, dbName);
             s.click(CREATE_DATABASE_ADVANCED_BUTTON_IMAGE);
@@ -34,40 +31,19 @@ public class Hello {
             Hello.selectAllPaste(s, BINARY_DATA_PATH_INPUT_IMAGE, folder + dbName + "/__DB__/data/" + dbName);
             s.click(ADD_STORAGE_BUTTON_IMAGE);
             Hello.selectAllPaste(s, STORAGE_PATH_INPUT_IMAGE, folder + dbName + "/__STORAGE__/");
-            Hello.selectAllPaste(s, STORAGE_DESCRIPTION_INPUT_IMAGE, DB_STORAGE_DESCRIPTION);
+            Hello.selectAllPaste(s, STORAGE_DESCRIPTION_INPUT_IMAGE, dbStorageDescription);
             s.click(ADD_STORAGE_OK_BUTTON_IMAGE);
             s.click(CREATE_DATABASE_OK_BUTTON_IMAGE);
-
         } catch (FindFailed e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     } // addDB
-
 
     public static <PFRML> void selectAllPaste(Screen s, PFRML target, String message) throws FindFailed {
         s.click(target);
         s.type("a", KeyModifier.CTRL);
         s.paste(message);
     } // selectAllPaste
-
-
-    public static void addDB(Screen s) {
-        try {
-            s.paste(ADD_DB_BUTTON_IMAGE, DB_NAME);
-            s.click(CREATE_DATABASE_ADVANCED_BUTTON_IMAGE);
-            Hello.selectAllPaste(s, FLOW_HISTORY_PATH_INPUT_IMAGE, DB_STRUCTURE_PATH + DB_NAME);
-            Hello.selectAllPaste(s, BINARY_DATA_PATH_INPUT_IMAGE, DB_DATA_PATH + DB_NAME);
-            s.click(ADD_STORAGE_BUTTON_IMAGE);
-            Hello.selectAllPaste(s, STORAGE_PATH_INPUT_IMAGE, DB_STORAGE_PATH);
-            Hello.selectAllPaste(s, STORAGE_DESCRIPTION_INPUT_IMAGE, DB_STORAGE_DESCRIPTION);
-            s.click(ADD_STORAGE_OK_BUTTON_IMAGE);
-            s.click(CREATE_DATABASE_OK_BUTTON_IMAGE);
-
-        } catch (FindFailed e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    } // addDB
 
 } // class Hello
